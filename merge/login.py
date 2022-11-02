7a4f49fe7d2e2017140b0b88bbb4691f25bc661a
smilerainlz <350948908@qq.com>
2022-11-02 22:21:44 +0800

    add merge

--- merge/login.py
+++ merge/login.py
@@ -1,148 +1,131 @@
-import os,wda,codecs,time
+import os, wda, codecs, time
 
-def addFirend(d):
-    d.click(0.63, 0.145)
-    d.send_keys("83881191")
-    d(label="search").click()
+
+def addFirend(client, appName):
+    client.click(0.63, 0.145)
+    client.send_keys("83881191")
+    client(label="search").click()
     time.sleep(1)
-    d.click(0.26, 0.148)
+    client.click(0.26, 0.148)
     time.sleep(1)
-    d.click(0.106, 0.307)
-    d.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[3]/Button[1]/StaticText[1]').click()
-    d(label="orangy ic profile back icon").click()
+    client.click(0.106, 0.307)
+    client.xpath(
+        '//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[3]/Button[1]/StaticText[1]').click()
+    if appName == "cm":
+        client(label="orangy ic profile back icon").click()
+    if appName == "ppx":
+        client(label="ppx ic profile back icon").click()
     time.sleep(1)
-    d.click(0.893, 0.094)
+    client.click(0.893, 0.094)
 
-def removeGZ(d):
-    d(label="我的").click()
-    d.xpath('//Table/Other[1]/Other[2]/Other[3]').click()
-    while True :
-        d.xpath('//Table/Button[2]/StaticText[1]').click()
-        d.xpath('//NavigationBar/Button[2]/StaticText[1]').click()
-        d.xpath('//Table/Cell[2]/Button[1]').click()
-        d.xpath('//Table/Cell[3]/Button[1]').click()
-        d.xpath('//Table/Cell[4]/Button[1]').click()
-        d.xpath('//Table/Cell[5]/Button[1]').click()
-        d.xpath('//Table/Cell[6]/Button[1]').click()
-        d.xpath('//Table/Cell[7]/Button[1]').click()
-        d.xpath('//Table/Cell[8]/Button[1]').click()
-        d.xpath('//Table/Cell[9]/Button[1]').click()
-        d.xpath('//Table/Cell[10]/Button[1]').click()
-        d.xpath('//NavigationBar/Button[2]/StaticText[1]').click()
-        d.xpath('//Window[1]/Other[2]/Other[2]/Button[3]/StaticText[1]').click()
 
-def test_preferences_ppx(client,username):
-    #print("Status:", client.status())
-    #print("Info:", client.info)
-    #print("BatteryInfo", client.battery_info())
-    #print("AppCurrent:", client.app_current())
-    print("当前时间: %s" % time.ctime())
-    print(username)
-
-    if not client(className="XCUIElementTypeOther").accessible :
-    	client.click(0.09, 0.893)
-    client(label="ppx ic UserName Login Icon").click()
-    client(className="XCUIElementTypeTextField").clear_text()
-    client(className="XCUIElementTypeTextField").set_text(username)
-    client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Button[1]/StaticText[1]').click()
-    client(className="XCUIElementTypeSecureTextField").set_text("qwe12345")
-    client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[2]').click()
-    time.sleep(2)
-    if client(label="确定").exists :
-        client(label="确定").click()
-        time.sleep(3)
-        client(label="ppx ic UserName Login Icon").click()
-        client(className="XCUIElementTypeTextField").clear_text()
-        client(className="XCUIElementTypeTextField").set_text(username)
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Button[1]/StaticText[1]').click()
-        client(className="XCUIElementTypeSecureTextField").set_text("qwe12345")
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[2]').click()
-        time.sleep(2)
-    if client(label="daily reward close").exists :
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Other[1]/Image[1]/Image[1]').click()
-        time.sleep(3)
-        client(label="daily reward close").click()
+def removeGZ(client):
     client(label="我的").click()
-    client(label="ppx ic hl me page setting icon").click()
-    client.xpath('//ScrollView/Button[10]').click()
-    client.xpath('//Window[1]/Other[2]/Other[1]/Other[1]/Button[1]/StaticText[1]').click()
-    time.sleep(1)
+    client.xpath('//Table/Other[1]/Other[2]/Other[3]').click()
+    while True:
+        client.xpath('//Table/Button[2]/StaticText[1]').click()
+        client.xpath('//NavigationBar/Button[2]/StaticText[1]').click()
+        client.xpath('//Table/Cell[2]/Button[1]').click()
+        client.xpath('//Table/Cell[3]/Button[1]').click()
+        client.xpath('//Table/Cell[4]/Button[1]').click()
+        client.xpath('//Table/Cell[5]/Button[1]').click()
+        client.xpath('//Table/Cell[6]/Button[1]').click()
+        client.xpath('//Table/Cell[7]/Button[1]').click()
+        client.xpath('//Table/Cell[8]/Button[1]').click()
+        client.xpath('//Table/Cell[9]/Button[1]').click()
+        client.xpath('//Table/Cell[10]/Button[1]').click()
+        client.xpath('//NavigationBar/Button[2]/StaticText[1]').click()
+        client.xpath('//Window[1]/Other[2]/Other[2]/Button[3]/StaticText[1]').click()
 
-def test_preferences_cm(client,username):
-    #print("Status:", client.status())
-    #print("Info:", client.info)
-    #print("BatteryInfo", client.battery_info())
-    #print("AppCurrent:", client.app_current())
+
+def login(client, username, appName):
     print("当前时间: %s" % time.ctime())
     print(username)
-    if not client(className="XCUIElementTypeOther").accessible :
-    	client.click(0.09, 0.893)
-    client(label="orangy ic UserName Login Icon").click()
-    client(className="XCUIElementTypeTextField").clear_text()
-    client(className="XCUIElementTypeTextField").set_text(username)
-    client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Button[1]/StaticText[1]').click()
-    client(className="XCUIElementTypeSecureTextField").set_text("qwe12345")
-    client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[2]').click()
-    time.sleep(2)
-    if client(label="确定").exists :
-        client(label="确定").click()
-        time.sleep(3)
+    # 如果未勾选用户协议，勾选
+    if not client(className="XCUIElementTypeOther").accessible:
+        client.click(0.09, 0.893)
+    # 选择用户名登录
+    if appName == "cm":
         client(label="orangy ic UserName Login Icon").click()
-        client(className="XCUIElementTypeTextField").clear_text()
-        client(className="XCUIElementTypeTextField").set_text(username)
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Button[1]/StaticText[1]').click()
-        client(className="XCUIElementTypeSecureTextField").set_text("qwe12345")
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[2]').click()
-        time.sleep(2)
-    if client(label="daily reward close").exists :
-        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Other[1]/Image[1]/Image[1]').click()
-        time.sleep(3)
-        client(label="daily reward close").click()
-    #addFirend(client)
-    client(label="我的").click()
-    client(label="orangy ic hl me page setting i").click()
-    client.xpath('//ScrollView/Button[10]').click()
-    client.xpath('//Window[1]/Other[2]/Other[1]/Other[1]/Button[1]/StaticText[1]').click()
-    time.sleep(1)
+    if appName == "ppx":
+        client(label="ppx ic UserName Login Icon").click()
+    # 清空用户名并输入用户名
+    client(className="XCUIElementTypeTextField").clear_text()
+    client(className="XCUIElementTypeTextField").set_text(username)
+    # 点击密码输入框并输入密码
+    client.xpath(
+        '//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Button[1]/StaticText[1]').click()
+    client(className="XCUIElementTypeSecureTextField").set_text("qwe12345")
+    # 点击登录
+    client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[2]').click()
 
-runCount=0
+
+def close(client, appName):
+    # 如果弹出签到提示，先签到，再关闭
+    if client(label="daily reward close").exists:
+        client.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Other[1]/Image[1]/Image[1]').click()
+        time.sleep(1)
+        client(label="daily reward close").click()
+    # 如果提示年度活动，关掉
+    if appName == "cm":
+        if client(label="orangy ic main activity close").exists:
+            client(label="orangy ic main activity close").click()
+    if appName == "ppx":
+        if client(label="ppx ic main activity close").exists:
+            client(label="ppx ic main activity close").click()
+    # 退出
+    if client(label="我的").exists:
+        client(label="我的").click()
+        if appName == "cm":
+            client(label="orangy ic hl me page setting i").click()
+        if appName == "ppx":
+            client(label="ppx ic hl me page setting icon").click()
+        client.xpath('//ScrollView/Button[10]').click()
+        client.xpath('//Window[1]/Other[2]/Other[1]/Other[1]/Button[1]/StaticText[1]').click()
+
+
+def test_preferences(client, username, appName):
+    # 登录
+    login(client, username, appName)
+    # 如果登录过期
+    if client(label="确定").exists:
+        client(label="确定").click()
+        # 重新登录
+        login(client, username, appName)
+    time.sleep(3)
+    close(client, appName)
+
+
+runCount = 0
 myclient = wda.USBClient()
-while True :
+while True:
+    # 打开cm
     myclient.session().app_terminate("sg.bigo.orangy")
     myclient.session().app_activate("sg.bigo.orangy")
-    time.sleep(5)
-    if myclient(label="daily reward close").exists :
-        myclient.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Other[1]/Image[1]/Image[1]').click()
-        time.sleep(3)
-        myclient(label="daily reward close").click()
-    if myclient(label="我的").exists :
-        myclient(label="我的").click()
-        myclient(label="orangy ic hl me page setting i").click()
-        myclient.xpath('//ScrollView/Button[10]').click()
-        myclient.xpath('//Window[1]/Other[2]/Other[1]/Other[1]/Button[1]/StaticText[1]').click()
+    if myclient(label="确定").exists:
+        myclient(label="确定").click()
+    # 初始化一次，如果未退出就退出
+    close(myclient, "cm")
     runCount = runCount + 1
-    if runCount%10==0:
+    # 循环超过10次重启charles
+    if runCount % 10 == 0:
         os.system('sh ../charles-start.sh')
     file_cm = codecs.open("username_cm.txt", 'r', "utf-8")
     for line in file_cm:
-        test_preferences_cm(myclient,line)
+        test_preferences(myclient, line, "cm")
     file_cm.close()
-    time.sleep(1)
-     
+    time.sleep(3)
+
+    # 打开ppx
     myclient.session().app_terminate("sg.bigo.pipixia")
     myclient.session().app_activate("sg.bigo.pipixia")
-    time.sleep(5)
-    if myclient(label="daily reward close").exists :
-        myclient.xpath('//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[2]/Other[1]/Image[1]/Image[1]').click()
-        time.sleep(3)
-        myclient(label="daily reward close").click()
-    if myclient(label="我的").exists :
-        myclient(label="我的").click()
-        myclient(label="ppx ic hl me page setting icon").click()
-        myclient.xpath('//ScrollView/Button[10]').click()
-        myclient.xpath('//Window[1]/Other[2]/Other[1]/Other[1]/Button[1]/StaticText[1]').click()
+    if myclient(label="确定").exists:
+        myclient(label="确定").click()
+    # 初始化一次，如果未退出就退出
+    close(myclient, "ppx")
+
     file_ppx = codecs.open("username_ppx.txt", 'r', "utf-8")
     for line in file_ppx:
-        test_preferences_ppx(myclient,line)
+        test_preferences(myclient, line, "ppx")
     file_ppx.close()
-    time.sleep(1)
+    time.sleep(3)
