cf6b8293ac38be4d9c6035620880f87f713f4d93
smilerainlz <350948908@qq.com>
2022-12-05 21:59:22 +0800

    update

--- recharge/recharge.py
+++ recharge/recharge.py
@@ -1,21 +1,43 @@
-import os, wda, codecs, time, sys
+import wda, codecs, time
 
-def process(client,userId):
+def process(d,userId):
     print(userId)
-    client.xpath('//*[@label=""]').click()
-    client.xpath('//*[@label=""]').clear_text()
-    client.send_keys(userId)
-    client(label="完成").click()
-    time.sleep(30)
+    d.home()
+    d(label="Safari浏览器").click()
+    time.sleep(2)
+    if d(label="取消").exists:
+        d(label="取消").click()
+    if d(label="确定").exists:
+        d(label="确定").click()
+    d.xpath('//*[@label=""]').click()
+    d.xpath('//*[@label=""]').clear_text()
+    d.send_keys(userId)
+    d(label="完成").click()
+    time.sleep(2)
+    if d(className="XCUIElementTypeSwitch").value == "0":
+        d.xpath('//Switch').click()
+    d.swipe(0.5, 0.5, 0.5, 0.01)
+    d(label="微信支付").click()
+    time.sleep(2)
+    if d(label="打开").exists:
+        d(label="打开").click()
+    d(label="立即支付").click()
+    if d(label="确认支付").exists:
+        d(label="确认支付").click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[8]/StaticText[1]').click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[6]/StaticText[1]').click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[10]/StaticText[1]').click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[8]/StaticText[1]').click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[2]/StaticText[1]').click()
+    d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[2]/StaticText[1]').click()
+    d(label="完成").click()
 
 # 本机
-# myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
+myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
 # 测试机器
-myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
+# myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
 # ipad
 # myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
-myclient.home()
-myclient(label="Safari浏览器").click()
 while True:
     file = codecs.open("first.txt", 'r', "utf-8")
     for line in file:
