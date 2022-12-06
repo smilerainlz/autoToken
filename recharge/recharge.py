c88067dede47d83208469b0f3ecae02449f52278
smilerainlz <350948908@qq.com>
2022-12-06 22:56:00 +0800

    update

--- recharge/recharge.py
+++ recharge/recharge.py
@@ -1,6 +1,7 @@
 import wda, codecs, time
 
-def process(d,userId):
+
+def process(d, userId):
     print(userId)
     d.home()
     d(label="Safari浏览器").click()
@@ -14,6 +15,7 @@ def process(d,userId):
     d.send_keys(userId)
     d(label="完成").click()
     time.sleep(2)
+    d.xpath('//*[@label="充值中心"]/Other[12]').click()
     if d(className="XCUIElementTypeSwitch").value == "0":
         d.xpath('//Switch').click()
     d.swipe(0.5, 0.5, 0.5, 0.01)
@@ -21,6 +23,12 @@ def process(d,userId):
     time.sleep(2)
     if d(label="打开").exists:
         d(label="打开").click()
+    print(d(className="XCUIElementTypeStaticText")[2].value)
+    print('购买Hello语音钻石到账号ID' + userId.strip().replace(' ', '').replace('\n', ''))
+    if d(className="XCUIElementTypeStaticText")[2].value != (
+            '购买Hello语音钻石到账号ID' + userId.strip().replace(' ', '').replace('\n', '')):
+        print("充值账号错误，无限睡眠：" + d(className="XCUIElementTypeStaticText")[2].value)
+        time.sleep(5000000)
     d(label="立即支付").click()
     if d(label="确认支付").exists:
         d(label="确认支付").click()
@@ -32,14 +40,15 @@ def process(d,userId):
     d.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Key[2]/StaticText[1]').click()
     d(label="完成").click()
 
+
 # 本机
-myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
+myclient = wda.USBClient("00008101-000958911A91001E", port=8100)
 # 测试机器
 # myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
 # ipad
 # myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
 while True:
-    file = codecs.open("first.txt", 'r', "utf-8")
+    file = codecs.open("second.txt", 'r', "utf-8")
     for line in file:
         process(myclient, line)
     file.close()
