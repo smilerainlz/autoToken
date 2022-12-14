42c2c19310955b21778952b4a6c2ab893abebf60
smilerainlz <350948908@qq.com>
2022-12-14 23:35:21 +0800

    update

---
+++ blindbox/blindbox.py
@@ -0,0 +1,21 @@
+import os, wda, codecs, time, sys
+
+sys.path.append("..//")
+import hello
+
+runCount = 0
+myclient = wda.USBClient()
+while True:
+    runCount = runCount + 1
+    # 循环超过5次重启charles
+    if runCount % 5 == 0:
+        os.system('sh ../charles-start.sh')
+        time.sleep(10)
+    # 打开cm
+    hello.init(myclient)
+
+    file_cm = codecs.open("../data/blindbox.txt", 'r', "utf-8")
+    for line in file_cm:
+        hello.process(myclient, line, "ipad", "username", "false", 2)
+    file_cm.close()
+    time.sleep(3)
