efc07b545b084bef8afdf690673d6b769efba49d
smilerainlz <350948908@qq.com>
2022-12-14 23:30:29 +0800

    update

--- all-first/myPhoneNew-first-part4.py
+++ all-first/myPhoneNew-first-part4.py
@@ -5,11 +5,13 @@ import hello
 
 runCount = 0
 # 本机
-myclient = wda.USBClient("00008101-000958911A91001E", port=8100)
+# myclient = wda.USBClient("00008101-000958911A91001E", port=8100)
 # 测试机器
 # myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
 # ipad
 # myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
+# 本机（新）
+myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
 while True:
     runCount = runCount + 1
     # 循环超过5次重启charles
--- myNewPhone/myNewPhone-first.py
+++ myNewPhone/myNewPhone-first.py
@@ -7,11 +7,11 @@ runCount = 0
 # 本机
 # myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
 # 测试机器
-myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
+# myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
 # ipad
 # myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
 # 本机（新）
-# myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
+myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
 while True:
     runCount = runCount + 1
     # 循环超过5次重启charles
@@ -24,6 +24,6 @@ while True:
 
     file_cm = codecs.open("../data/first.txt", 'r', "utf-8")
     for line in file_cm:
-        hello.process(myclient, line, "testPhone", "username", "false", 2)
+        hello.process(myclient, line, "myPhone", "username", "false", 2)
     file_cm.close()
     time.sleep(3)
