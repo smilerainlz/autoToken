2073212faa846b6a8931acfe7615571b32a31d2b
smilerainlz <350948908@qq.com>
2022-12-15 11:15:10 +0800

    update

--- hello.py
+++ hello.py
@@ -150,12 +150,12 @@ def getSecurityPacket(client, isGetSecurityPacket, device):
     if device != "ipad":
         client.swipe(0.5, 0.88, 0.5, 0.1)
     time.sleep(1)
-    client.xpath('//Table/Cell[4]/Image[1]').click()
+    client.xpath('//Table/Cell[5]/Image[1]').click()
     time.sleep(1)
     if isGetSecurityPacket == 1:
         for i in range(7):
             client(label="orangy ic common back black").click()
-            client.xpath('//Table/Cell[4]/Image[1]').click()
+            client.xpath('//Table/Cell[5]/Image[1]').click()
             time.sleep(120)
     client(label="orangy ic common back black").click()
 
