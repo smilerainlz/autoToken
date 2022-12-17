e51c031576c45fd4a3f5972fc6adede179680540
smilerainlz <350948908@qq.com>
2022-12-17 09:42:46 +0800

    update

--- hello.py
+++ hello.py
@@ -145,19 +145,17 @@ def getDiamond(client):
 
 def getSecurityPacket(client, isGetSecurityPacket, device):
     client(label="发现").click()
-    # if device == "ipad":
-    #    client.swipe(0.5, 0.88, 0.5, 0.1)
-    # if device != "ipad":
-    #    client.swipe(0.5, 0.88, 0.5, 0.1)
-    # time.sleep(1)
-    client.swipe(0.5, 0.8, 0.5, 0.3)
+    if device == "ipad":
+        client.swipe(0.5, 0.88, 0.5, 0.3)
+    else:
+        client.swipe(0.5, 0.88, 0.5, 0.5)
     time.sleep(1)
-    client.xpath('//Table/Cell[3]/Image[1]').click()
+    client.xpath('//Table/Cell[2]/Image[1]').click()
     time.sleep(2)
     if isGetSecurityPacket == 1:
         for i in range(7):
             client(label="orangy ic common back black").click()
-            client.xpath('//Table/Cell[3]/Image[1]').click()
+            client.xpath('//Table/Cell[2]/Image[1]').click()
             time.sleep(120)
     client(label="orangy ic common back black").click()
 
