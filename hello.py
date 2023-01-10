404a3f88b85328f88a7106aaad2fcba8f748b806
garylizheng <garylizheng@didiglobal.com>
2023-01-10 11:20:00 +0800

    update

--- hello.py
+++ hello.py
@@ -142,24 +142,31 @@ def getDiamond(client):
 
 def getSecurityPacket(client, isGetSecurityPacket, device):
     client(label="发现").click()
-    if device == "ipad":
+    # if device == "ipad":
         # 2
         # client.swipe(0.5, 0.88, 0.5, 0.5)
         # 3
-        client.swipe(0.5, 0.88, 0.5, 0.3)
+        # client.swipe(0.5, 0.88, 0.5, 0.3)
         # 4
         # client.swipe(0.5, 0.88, 0.5, 0.5)
         # client.swipe(0.5, 0.88, 0.5, 0.5)
-    else:
+    # else:
         # 2
         # client.swipe(0.5, 0.88, 0.5, 0.7)
         # 3
-        client.swipe(0.5, 0.88, 0.5, 0.5)
+        # client.swipe(0.5, 0.88, 0.5, 0.5)
         # 4
         # client.swipe(0.5, 0.88, 0.5, 0.2)
-    time.sleep(1)
-    client.xpath('//Table/Cell[3]/Image[1]').click()
-    time.sleep(2)
+    time.sleep(3)
+    client.xpath('//Table/Cell[1]/Image[1]').click()
+    time.sleep(3)
+    client.swipe(0.5, 0.88, 0.5, 0.5)
+    client.swipe(0.5, 0.88, 0.5, 0.5)
+    time.sleep(3)
+    client(label="前往购买礼包").click()
+    time.sleep(3)
+    client(label="60钻购买").click()
+    client.xpath('//*[@label="网页对话框"]/Other[1]').click()
     if isGetSecurityPacket == 1:
         for i in range(7):
             client(label="orangy ic common back black").click()
