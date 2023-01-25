f533cdc1784e78d1c02d52acc65964de4961b20c
garylizheng <garylizheng@didiglobal.com>
2023-01-25 21:05:26 +0800

    update

--- blindbox/blindbox.py
+++ blindbox/blindbox.py
@@ -4,7 +4,14 @@ sys.path.append("..//")
 import hello
 
 runCount = 0
-myclient = wda.USBClient()
+# 本机
+# myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
+# 测试机器
+myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
+# ipad
+# myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
+# 本机（新）
+# myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
 while True:
     runCount = runCount + 1
     # 循环超过5次重启charles
@@ -16,6 +23,6 @@ while True:
 
     file_cm = codecs.open("../data/blindbox.txt", 'r', "utf-8")
     for line in file_cm:
-        hello.process(myclient, line, "ipad", "username", "true", 0)
+        hello.process(myclient, line, "testPhone", "username", "true", 0)
     file_cm.close()
     time.sleep(3)
