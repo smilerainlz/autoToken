3535ccb5ed7c8dc77477199942fc8d4c2f0b13f2
lizheng <garylizheng@didiglobal.com>
2023-03-01 00:47:03 +0800

    update

--- lottery-ipad/lottery-ipad.sh
+++ lottery-ipad/lottery-ipad.sh
@@ -1,9 +1,9 @@
 while true
 do
-  loginCount=`ps -ef | grep blindbox-ipad.py | wc -l`
+  loginCount=`ps -ef | grep lottery-ipad.py | wc -l`
   if (($loginCount < 2))
   then
-    python3 blindbox-ipad.py
+    python3 lottery-ipad.py
   fi
   sleep 1
 done
