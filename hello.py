b56141e977781d083e86853f91366d3058378a9b
lizheng <garylizheng@didiglobal.com>
2023-03-03 10:06:51 +0800

    update

--- hello.py
+++ hello.py
@@ -159,7 +159,7 @@ def getSecurityPacket(client, isGetSecurityPacket, device):
     # 4
     client.swipe(0.5, 0.88, 0.5, 0.2)
     time.sleep(3)
-    client.xpath('//Table/Cell[1]/Image[1]').click()
+    client.xpath('//Table/Cell[4]/Image[1]').click()
     time.sleep(2)
     client.swipe(0.5, 0.8, 0.5, 0.5)
     time.sleep(2)
