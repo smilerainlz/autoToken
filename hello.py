badabcd51c22ab512c4c896c6d286ad61166069b
lizheng <350948908@qq.com>
2023-08-04 23:51:07 +0800

    update

--- hello.py
+++ hello.py
@@ -154,18 +154,7 @@ def getSecurityPacket(client, isGetSecurityPacket, device, appType):
     if appType == "hello":
         client(label="星球").click()
     time.sleep(5)
-    client.swipe(0.5, 0.88, 0.5, 0.2)
-    client.swipe(0.5, 0.88, 0.5, 0.3)
-    client.swipe(0.5, 0.88, 0.5, 0.5)
-    if device == "ipad":
-        client.swipe(0.5, 0.88, 0.5, 0.2)
-    if appType == "hello":
-        client.swipe(0.5, 0.88, 0.5, 0.3)
-    time.sleep(3)
-    if appType == "hello":
-        client.xpath('//Table/Cell[8]/Image[1]').click()
-    if appType != "hello":
-        client.xpath('//Table/Cell[7]/Image[1]').click()
+    client(label="超级玩家").click()
     time.sleep(2)
     client.swipe(0.5, 0.8, 0.5, 0.5)
     time.sleep(2)
