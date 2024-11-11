eccaf157cba8abab15eb5f6b8b7a3b77510256e4
lizheng <350948908@qq.com>
2024-11-11 18:17:44 +0800

    update

--- getSecurityPacketForOne/testPhone001.py
+++ getSecurityPacketForOne/testPhone001.py
@@ -4,7 +4,7 @@ sys.path.append("..//")
 import hello
 
 uuid = "08771801cd4ba47bd323204310a296c6e05e2b1a"
-key = "0034"
+key = "0035"
 
 # hello.doAll(uuid, key)
 hello.doFromFile(uuid, key, "001", "hello")
--- getSecurityPacketForOne/testPhone011.py
+++ getSecurityPacketForOne/testPhone011.py
@@ -4,7 +4,7 @@ sys.path.append("..//")
 import hello
 
 uuid = "04f53f637d3dd14f7266fcb483256bc81080e235"
-key = "0035"
+key = "0037"
 
 hello.doAll(uuid, key)
 #hello.doFromFile(uuid, key, "011", "hello")
