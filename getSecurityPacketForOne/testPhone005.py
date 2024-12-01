7ba8558187362a993f084bf544bab727aa53e596
lizheng <350948908@qq.com>
2024-12-01 21:20:57 +0800

    update

--- getSecurityPacketForOne/testPhone005.py
+++ getSecurityPacketForOne/testPhone005.py
@@ -4,7 +4,7 @@ sys.path.append("..//")
 import hello
 
 uuid = "77df71489bd1227029fad1ce678d80490f52196e"
-key = "0036"
+key = "0037"
 
 # hello.doAll(uuid, key)
 hello.doFromFile(uuid, key, "005", "hello")
