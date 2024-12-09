cf59555839a43db46abb321ea40d054063c5cfe8
lizheng <350948908@qq.com>
2024-12-09 12:49:26 +0800

    update

--- getSecurityPacketForOne/testPhone002.py
+++ getSecurityPacketForOne/testPhone002.py
@@ -6,6 +6,6 @@ import hello
 uuid = "c933eea94b3ba611f48322b9d9ebc02e9c1efef1"
 key = "0042"
 
-# hello.doAll(uuid, key)
-hello.doFromFile(uuid, key, "002", "hello", "true")
+hello.doAll(uuid, key)
+# hello.doFromFile(uuid, key, "002", "hello", "true")
 #hello.doFromFileAll(uuid, key, "002")
