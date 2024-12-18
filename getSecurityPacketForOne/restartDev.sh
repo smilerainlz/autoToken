ed5b908e08ede0b2178d024dba3456c4f85015de
lizheng <350948908@qq.com>
2024-12-18 23:43:24 +0800

    update

--- getSecurityPacketForOne/restartDev.sh
+++ getSecurityPacketForOne/restartDev.sh
@@ -1,1 +1,2 @@
-/Users/jfx/Library/Python/3.9/bin/tidevice -u 08771801cd4ba47bd323204310a296c6e05e2b1a kill com.facebook.WebDriverAgentLib.lizhengtest0045.xctrunner
+/Users/jfx/Library/Python/3.9/bin/tidevice -u 77df71489bd1227029fad1ce678d80490f52196e kill com.facebook.WebDriverAgentLib.lizhengtest0046.xctrunner
+/Users/jfx/Library/Python/3.9/bin/tidevice -u 04f53f637d3dd14f7266fcb483256bc81080e235 kill com.facebook.WebDriverAgentLib.lizhengtest0046.xctrunner
