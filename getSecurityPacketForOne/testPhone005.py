640e669828d8a673506640d351089d681588b9a8
lizheng <lizheng@ccbscf.com>
2024-12-24 16:34:35 +0800

    update

--- getSecurityPacketForOne/testPhone005.py
+++ getSecurityPacketForOne/testPhone005.py
@@ -7,4 +7,4 @@ uuid = "77df71489bd1227029fad1ce678d80490f52196e"
 key = "0046"
 
 # hello.doAll(uuid, key)
-hello.doFromFile(uuid, key, "005", "hello", "true")
+hello.doFromFile(uuid, key, "005", "hello", "false")
