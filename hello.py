eed7263a5e61e62c3ddac438e5b9111dd54aa00c
lizheng <350948908@qq.com>
2025-03-23 10:32:50 +0800

    update

--- hello.py
+++ hello.py
@@ -321,19 +321,19 @@ def doFromFile(uuid, key, fileName, appType, isCheckDiamond):
                 print(line.split("----")[2])
                 try:
                     process(myclient, line.split("----")[0], line.split("----")[1], line.split("----")[2],
-                            isCheckDiamond, 0,
+                            isCheckDiamond, 1,
                             appType)
                 except:
                     init(myclient, appType)
                     try:
                         process(myclient, line.split("----")[0], line.split("----")[1], line.split("----")[2],
                                 isCheckDiamond,
-                                0, appType)
+                                1, appType)
                     except:
                         init(myclient, appType)
                         process(myclient, line.split("----")[0], line.split("----")[1], line.split("----")[2],
                                 isCheckDiamond,
-                                0,
+                                1,
                                 appType)
             time.sleep(1200)
 
