93e52e627e3dcf2aa8d0d96cb0c3b15a54aac6f1
lizheng <350948908@qq.com>
2025-03-23 10:19:08 +0800

    update

--- getSecurityPacketForOne/myNewPhone.py
+++ getSecurityPacketForOne/myNewPhone.py
@@ -1,26 +1,11 @@
-import os, wda, codecs, time, sys
+import sys
 
 sys.path.append("..//")
 import hello
 
-runCount = 0
-# 本机
-# myclient = wda.USBClient("00008101-000958911A91001E",port=8100)
-# 测试机器
-# myclient = wda.USBClient("00008101-0006310A0C32001E", port=8100)
-# ipad
-# myclient = wda.USBClient("00008030-001E245A21C0202E", port=8100)
-# 本机（新）
-myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
-while True:
-    runCount = runCount + 1
-    # 循环超过5次重启charles
-    if runCount % 50000 == 0:
-        os.system('sh ../charles-start.sh')
-        time.sleep(10)
+uuid = "00008110-001A21803482401E"
+key = "0049"
 
-    # 打开cm
-    hello.initNoClose(myclient, "hello")
-    # hello.process(myclient, "q350948908", "phone", "username", "false", 1, "hello")
-    hello.processNew(myclient, "q3452725271", "myPhone", "username", "false", 1, "hello")
-    time.sleep(30)
+# hello.doAll(uuid, key)
+hello.doFromFile(uuid, key, "001", "hello", "false")
+# hello.doFromFileAll(uuid, key, "001")
