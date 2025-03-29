c384f49dbd1f8dc794b292826f80e36351f84c63
lizheng <350948908@qq.com>
2025-03-29 18:05:21 +0800

    update

--- getSecurityPacketForOne/demo.py
+++ getSecurityPacketForOne/demo.py
@@ -1,4 +1,4 @@
-import sys,wda
+import sys, wda
 import time
 
 sys.path.append("..//")
@@ -7,28 +7,49 @@ import hello
 uuid = "00008110-001A21803482401E"
 key = "0052"
 
+myclient = wda.USBClient(uuid, port=8100)
+
+# 开超级玩家
+myclient.xpath("//*[@label=\"星球\"]").click()
+myclient.xpath("//*[@label=\"超级玩家\"]").click()
+myclient.swipe_up()
+myclient.swipe(0.7, 0.758, 0.204, 0.768)
+myclient.xpath("//*[@label=\"¥3\"]").click()
+myclient.xpath("//*[@label=\"立即开通 体验7天\"]").click()
+myclient.xpath("//*[@label=\"订阅\"]").click()
+time.sleep(5)
+if myclient(label="购买").exists:
+    myclient.xpath("//*[@label=\"购买\"]").click()
+    myclient.xpath("//*[@label=\"好\"]").click()
+    time.sleep(5)
+else:
+    myclient.send_keys("860822!Zzpg")
+    myclient.xpath("//*[@label=\"登录\"]").click()
+    myclient.xpath("//*[@label=\"购买\"]").click()
+    myclient.xpath("//*[@label=\"好\"]").click()
+myclient.xpath(
+    "//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/WebView[1]/WebView[1]/WebView[1]/Other[1]/Other[1]/Other[1]/Other[1]/Button[1]").click()
 
 # 送礼物
-myclient = wda.USBClient(uuid, port=8100)
-myclient.xpath("//*[@label=\"搜大神、搜房间、搜玩友\"]").click()
-myclient.send_keys("588815")
-myclient.xpath("//*[@label=\"搜索\"]").click()
-myclient.click(0.109, 0.291)
-myclient.xpath("//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[4]/Other[1]/Button[1]").click()
-myclient.xpath("//*[@label=\"送礼物\"]").click()
-#送普通礼物
-#myclient.xpath("//*[@label=\"星耀票\"]").click()
-#myclient.xpath("//*[@label=\"送礼\"]").click()
-#送福袋
-myclient.swipe_up()
-myclient.xpath("//*[@label=\"特别\"]").click()
-myclient.xpath("//*[@label=\"初级水晶球\"]").click()
-myclient.xpath("//*[@label=\"送礼\"]").click()
-myclient.xpath("//*[@label=\"new chatroom navi bar more\"]").click()
-myclient.xpath("//*[@label=\"new chatroom navi bar more\"]").click()
-myclient.xpath('//CollectionView/Cell[4]/Other[1]/Image[1]').click()
-time.sleep(1)
-myclient.click(0.901, 0.089)
+# myclient.xpath("//*[@label=\"搜大神、搜房间、搜玩友\"]").click()
+# myclient.send_keys("588815")
+# myclient.xpath("//*[@label=\"搜索\"]").click()
+# myclient.click(0.109, 0.291)
+# myclient.xpath("//Window[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[1]/Other[4]/Other[1]/Button[1]").click()
+# myclient.xpath("//*[@label=\"送礼物\"]").click()
+# 送普通礼物
+# myclient.xpath("//*[@label=\"星耀票\"]").click()
+# myclient.xpath("//*[@label=\"送礼\"]").click()
+# 送福袋
+# myclient.swipe_up()
+# myclient.xpath("//*[@label=\"特别\"]").click()
+# myclient.xpath("//*[@label=\"初级水晶球\"]").click()
+# myclient.xpath("//*[@label=\"送礼\"]").click()
+# myclient.xpath("//*[@label=\"new chatroom navi bar more\"]").click()
+# myclient.xpath("//*[@label=\"new chatroom navi bar more\"]").click()
+# myclient.xpath('//CollectionView/Cell[4]/Other[1]/Image[1]').click()
+# time.sleep(1)
+# myclient.click(0.901, 0.089)
 
 # 充值
 # myclient.click(0.343, 0.204)
