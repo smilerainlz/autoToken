d09a41d3a51a6bd8739a9791ccf818cfd5254f16
lizheng <350948908@qq.com>
2025-03-31 02:43:04 +0800

    update

--- getSecurityPacketForOne/testPhone001.py
+++ getSecurityPacketForOne/testPhone001.py
@@ -13,4 +13,4 @@ key = "0057"
 # hello.doFromFile(uuid, key, "001", "hello", "false", 1, 0)
 
 # 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
-hello.doFromFileSendGift(uuid, key, "001", "hello", "588815", "fudai", "初级水晶球")
+hello.doFromFileSendGift(uuid, key, "001", "hello", "49502575", "fudai", "初级水晶球")
