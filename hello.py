037a6cf5c6bb09777c6639a5f77e83b566a466ff
lizheng <350948908@qq.com>
2025-04-14 21:40:47 +0800

    update

--- hello.py
+++ hello.py
@@ -437,7 +437,7 @@ def process(client, username, password, loginType, isCheckDiamond, isGetSecurity
     if isInToRoom == "true":
         intoRoom(client)
     # addFirend(client)
-    time.sleep(10)
+    # time.sleep(10)
     close(client, "true")
 
 
