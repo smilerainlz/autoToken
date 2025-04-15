67a33626665d97a064f5cd4de14db5d362c7f582
lizheng <350948908@qq.com>
2025-04-15 23:10:02 +0800

    update

--- getSecurityPacketForOne/testPhone006.py
+++ getSecurityPacketForOne/testPhone006.py
@@ -4,7 +4,13 @@ sys.path.append("..//")
 import hello
 
 uuid = "15b6ddd0b40473b4c753ad9ff7dddad149cf6eb4"
-key = "0040"
+key = "0064"
 
-hello.doAll(uuid, key)
-#hello.doFromFile(uuid, key, "006", "hello", "false")
+# 登陆好的
+# hello.doAll(uuid, key)
+
+# 通过文件一个个登陆uuid, key, fileName, appType, isCheckDiamond, isGetSecurityPacket, sleepTime
+hello.doFromFile(uuid, key, "006", "hello", "false", 1, 1500, "false", "true")
+
+# 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
+# hello.doFromFileSendGift(uuid, key, "005", "hello", "49502575", "package", "初级水晶球")
