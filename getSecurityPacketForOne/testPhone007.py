09119b888223c650112d0abc354bbc7209bab1c5
lizheng <350948908@qq.com>
2025-04-20 22:36:14 +0800

    update

--- getSecurityPacketForOne/testPhone007.py
+++ getSecurityPacketForOne/testPhone007.py
@@ -6,5 +6,11 @@ import hello
 uuid = "984e38e08278e882dda3017fc916d873d457b13d"
 key = "0035"
 
+# 登陆好的
 # hello.doAll(uuid, key)
-hello.doFromFile(uuid, key, "007", "false")
+
+# 通过文件一个个登陆uuid, key, fileName, appType, isCheckDiamond, isGetSecurityPacket, sleepTime, isInToRoom
+hello.doFromFile(uuid, key, "007", "hello", "false", 1, 1500, "false","false")
+
+# 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
+# hello.doFromFileSendGift(uuid, key, "001", "hello", "49502575", "package", "初级水晶球")
--- getSecurityPacketForOne/testPhone008.py
+++ getSecurityPacketForOne/testPhone008.py
@@ -6,5 +6,11 @@ import hello
 uuid = "dfb8efa033b55e38bcda1c0b6ac9627451c7de42"
 key = "0035"
 
+# 登陆好的
 # hello.doAll(uuid, key)
-hello.doFromFile(uuid, key, "008", "false")
+
+# 通过文件一个个登陆uuid, key, fileName, appType, isCheckDiamond, isGetSecurityPacket, sleepTime, isInToRoom
+hello.doFromFile(uuid, key, "008", "hello", "false", 1, 1500, "false","false")
+
+# 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
+# hello.doFromFileSendGift(uuid, key, "001", "hello", "49502575", "package", "初级水晶球")
