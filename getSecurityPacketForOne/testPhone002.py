c3389322ee52a9dc22d3923db2d9731a448ce20b
lizheng <350948908@qq.com>
2025-05-02 11:47:54 +0800

    update

--- getSecurityPacketForOne/testPhone002.py
+++ getSecurityPacketForOne/testPhone002.py
@@ -10,7 +10,7 @@ key = "0068"
 # hello.doAll(uuid, key)
 
 # 通过文件一个个登陆uuid, key, fileName, appType, isCheckDiamond, isGetSecurityPacket, sleepTime
-hello.doFromFile(uuid, key, "002", "hello", "false", 1, 1500, "false","false")
+hello.doFromFile(uuid, key, "002", "hello", "false", 1, 1200, "false","false")
 
 # 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
 # hello.doFromFileSendGift(uuid, key, "001", "hello", "588815", "package", "星星票")
--- getSecurityPacketForOne/testPhone007.py
+++ getSecurityPacketForOne/testPhone007.py
@@ -10,7 +10,7 @@ key = "0071"
 # hello.doAll(uuid, key)
 
 # 通过文件一个个登陆uuid, key, fileName, appType, isCheckDiamond, isGetSecurityPacket, sleepTime, isInToRoom
-hello.doFromFile(uuid, key, "007", "hello", "false", 1, 1500, "false","false")
+hello.doFromFile(uuid, key, "007", "hello", "false", 1, 1200, "false","false")
 
 # 送礼物uuid, key, fileName, appType, sendUserId, sendType, sendName
 # hello.doFromFileSendGift(uuid, key, "001", "hello", "49502575", "package", "初级水晶球")
