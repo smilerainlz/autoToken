8d8fc81eba1fa97e615fbe5ae7954e80707be421
lizheng <350948908@qq.com>
2025-07-01 18:17:13 +0800

    update

---
+++ recharge/search.py
@@ -0,0 +1,19 @@
+import wda, codecs, time
+
+
+def process(client, userId):
+    if client(label="搜大神、搜房间、搜玩友").exists:
+        client(label="搜大神、搜房间、搜玩友").click()
+    client.send_keys(userId)
+    time.sleep(2)
+    client.click(0.67, 0.309)
+    time.sleep(3)
+    client(label="profile back icon").click()
+    client.click(0.908, 0.093)
+
+
+myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
+file = codecs.open("helloid.txt", 'r', "utf-8")
+for line in file:
+    process(myclient, line)
+file.close()
