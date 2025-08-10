4ed9010621f30afa671fe2d09fa5a0f8a7ecdc3c
lizheng <350948908@qq.com>
2025-08-10 21:59:09 +0800

    update

--- getSecurityPacketForOne/testPhone001.py
+++ getSecurityPacketForOne/testPhone001.py
@@ -4,7 +4,7 @@ sys.path.append("..//")
 import hello
 
 uuid = "08771801cd4ba47bd323204310a296c6e05e2b1a"
-key = "1020"
+key = "1025"
 
 # 登陆好的
 # hello.doAll(uuid, key)
--- getSecurityPacketForOne/testPhone011.py
+++ getSecurityPacketForOne/testPhone011.py
@@ -4,7 +4,7 @@ sys.path.append("..//")
 import hello
 
 uuid = "04f53f637d3dd14f7266fcb483256bc81080e235"
-key = "1023"
+key = "1025"
 
 # 登陆好的
 # hello.doAll(uuid, key)
