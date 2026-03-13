e9f0c1c952715a5e047a6a6d7ab6b7c84c260a34
lizheng <350948908@qq.com>
2026-03-13 22:23:40 +0800

    update

---
+++ hitSuperPlayer.py
@@ -0,0 +1,49 @@
+import wda, codecs, time
+
+
+def process(client, userId):
+    userId = userId.strip().replace(' ', '').replace('\n', '')
+    print(userId)
+    time.sleep(2)
+    if client(label="取消").exists:
+        client(label="取消").click()
+        time.sleep(3)
+        if client(label="取消").exists:
+            client(label="取消").click()
+    if client(label="确定").exists:
+        client(label="确定").click()
+    client.xpath('//*[@label=""]').click()
+    client.xpath('//*[@label=""]').clear_text()
+    client.send_keys(userId)
+    time.sleep(1)
+    if client.xpath("//*[@label=\"充值中心\"]/TextField").value == userId:
+        client(label="完成").click()
+        time.sleep(2)
+        client.xpath('//*[@label="充值中心"]/Other[16]').click()
+        myclient.swipe_up()
+        if client.xpath("//Switch").value == "0":
+            time.sleep(1)
+            client.xpath("//Switch").click()
+        client(label="微信支付").click()
+        time.sleep(3)
+        if client(label="打开").exists:
+            client(label="打开").click()
+        time.sleep(5)
+        # client(label="立即支付").click()
+        if client(label="确认支付").exists:
+            client(label="确认支付").click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[8]/StaticText[1]').click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[6]/StaticText[1]').click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[10]/StaticText[1]').click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[8]/StaticText[1]').click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[2]/StaticText[1]').click()
+        client.xpath('//Window[3]/Other[1]/Other[1]/Other[2]/Other[1]/Key[2]/StaticText[1]').click()
+        client(label="完成").click()
+    else:
+        print(userId + " : 充值id错误，跳过！")
+
+
+myclient = wda.USBClient("00008110-001A21803482401E", port=8100)
+for i in range(100):
+    myclient.click(0.566, 0.896)
+    time.sleep(60000)
