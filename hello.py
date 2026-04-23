503e3921772d2c6d0b894e9b396798685151c72a
lizheng <350948908@qq.com>
2026-04-23 19:41:08 +0800

    update

--- hello.py
+++ hello.py
@@ -479,7 +479,7 @@ def getSecurityPacket(client, isGetSecurityPacket, appType):
     time.sleep(3)
     client(label="超级玩家").click()
     time.sleep(5)
-    # client.click(0.566, 0.896)
+    client.click(0.566, 0.896)
     time.sleep(1)
     client.swipe(0.5, 0.8, 0.5, 0.5)
     time.sleep(3)
